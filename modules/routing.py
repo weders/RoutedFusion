b203000d2a2be85a90ec8d9bc554221666dcf14b
Silvan Weder <silvan.weder@inf.ethz.ch>
2020-08-19 16:02:31 +0200

    added unet

--- modules/routing.py
+++ modules/routing.py
@@ -1,10 +1,102 @@
 import torch
 
 
-class ConfidenceRouting(torch.nn.Module):
+class UNet(torch.nn.Module):
     """
     Basic UNet building block, calling itself recursively.
     Note that the final output does not have a ReLU applied.
+    """
+
+    def __init__(self, Cin, F, Cout, depth, batchnorms=True):
+
+        super().__init__()
+        self.F = F
+        self.depth = depth
+
+        if batchnorms:
+            self.pre = torch.nn.Sequential(
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(Cin, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.BatchNorm2d(F),
+                torch.nn.ReLU(),
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(F, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.BatchNorm2d(F),
+                torch.nn.ReLU(),
+            )
+
+            self.post = torch.nn.Sequential(
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(3 * F, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.BatchNorm2d(F),
+                torch.nn.ReLU(),
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(F, Cout, kernel_size=3, stride=1, padding=0),
+                torch.nn.BatchNorm2d(Cout),
+            )
+        else:
+            self.pre = torch.nn.Sequential(
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(Cin, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.ReLU(),
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(F, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.ReLU(),
+            )
+
+            self.post = torch.nn.Sequential(
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(3 * F, F, kernel_size=3, stride=1, padding=0),
+                torch.nn.ReLU(),
+                torch.nn.ReflectionPad2d(1),
+                torch.nn.Conv2d(F, Cout, kernel_size=3, stride=1, padding=0),
+                torch.nn.ReLU()
+            )
+
+        if depth > 1:
+            self.process = UNet(F, 2 * F, 2 * F, depth - 1, batchnorms=batchnorms)
+        else:
+            if batchnorms:
+                self.process = torch.nn.Sequential(
+                    torch.nn.ReflectionPad2d(1),
+                    torch.nn.Conv2d(F, 2 * F, kernel_size=3, stride=1, padding=0),
+                    torch.nn.BatchNorm2d(2 * F),
+                    torch.nn.ReLU(),
+                    torch.nn.ReflectionPad2d(1),
+                    torch.nn.Conv2d(2 * F, 2 * F, kernel_size=3, stride=1, padding=0),
+                    torch.nn.BatchNorm2d(2 * F),
+                    torch.nn.ReLU(),
+                )
+            else:
+                self.process = torch.nn.Sequential(
+                    torch.nn.ReflectionPad2d(1),
+                    torch.nn.Conv2d(F, 2 * F, kernel_size=3, stride=1, padding=0),
+                    torch.nn.ReLU(),
+                    torch.nn.ReflectionPad2d(1),
+                    torch.nn.Conv2d(2 * F, 2 * F, kernel_size=3, stride=1, padding=0),
+                    torch.nn.ReLU(),
+                )
+
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, data):
+
+        features = self.pre(data)
+        lower_scale = self.maxpool(features)
+        lower_features = self.process(lower_scale)
+        upsampled = torch.nn.functional.interpolate(lower_features, scale_factor=2, mode="bilinear",
+                                                    align_corners=False)
+        H = data.shape[2]
+        W = data.shape[3]
+        upsampled = upsampled[:, :, :H, :W]
+        output = self.post(torch.cat((features, upsampled), dim=1))
+
+        return output
+
+
+class ConfidenceRouting(torch.nn.Module):
+    """
+    Network for confidence routing in RoutedFusion.
     """
 
     def __init__(self, Cin, F, Cout, depth, batchnorms=True):
